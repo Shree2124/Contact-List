afe2fb7259b8f2f82dc1d05d5818f56908d22406
Shree2124 <shreealasande@gmail.com>
2024-06-06 14:24:50 +0530

    commit 8

--- server/contactList/contactList/settings.py
+++ server/contactList/contactList/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-import dj_database_url
-from decouple import config
+# import dj_database_url
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
