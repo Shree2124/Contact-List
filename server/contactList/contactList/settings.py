f0fff34e5db6d0730a0da7f4c60815f450ea8023
Shree2124 <shreealasande@gmail.com>
2024-06-06 14:02:12 +0530

    commit 4

--- server/contactList/contactList/settings.py
+++ server/contactList/contactList/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'contactList.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
