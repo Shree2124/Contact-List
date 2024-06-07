8b8acc04116b32751a2d28c41468e49b5661cf8c
Shree2124 <shreealasande@gmail.com>
2024-06-07 11:55:42 +0530

    commit 16

--- server/contactList/api/views.py
+++ server/contactList/api/views.py
@@ -21,7 +21,8 @@ def AddNumber(request):
     if serializer.is_valid():
         serializer.save()
         return Response({"success"},status=status.HTTP_201_CREATED)
-    return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    return JsonResponse({"message":serializer.error,
+                         "status":status.HTTP_400_BAD_REQUEST})
 
 @api_view(['DELETE'])
 def DeleteNumber(request, id):
